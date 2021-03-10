8e1d58f7988e173cb8bf1bfba3c52a398fc694aa
Scott Dixon <scott.dixon@shopify.com>
2021-03-10 15:39:40 +1000

    DropZone + generate staging URL

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,53 @@
-import { Heading, Page } from "@shopify/polaris";
+import React, {useState, useCallback} from "react";
+import { DropZone } from "@shopify/polaris";
+import { withApollo } from '@apollo/react-hoc';
+import { gql } from 'apollo-boost';
+import { useMutation } from "react-apollo";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const GENERATE_URL = gql`
+  mutation stagedUploadsCreate($input: [StagedUploadInput!]!) {
+    stagedUploadsCreate(input: $input) {
+      stagedTargets {
+        resourceUrl
+        url
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+function Index() {
+  const [file, setFile] = useState();
+
+  const uploadFile = (staged) => {
+    console.log(staged)
+    console.log('UPLOAD', file)
+    const formData = new FormData()
+    // Do the actual upload
+  }
+  const [generateUrl, { data }] = useMutation(GENERATE_URL, { onCompleted: uploadFile });
+
+  const handleDropZoneDrop = (files) => {
+    setFile(files[0])
+    generateUrl({ variables: {
+      "input": [
+        {
+          "resource": "COLLECTION_IMAGE",
+          "filename": files[0].name,
+          "mimeType": files[0].type
+        }
+      ]
+    }})
+  }
+
+  return (
+    <DropZone onDrop={handleDropZoneDrop} allowMultiple={false}>
+      <DropZone.FileUpload />
+    </DropZone>
+  );
+}
 
 export default Index;
