1b50876d5e3d74bb07f19b29b69c1fe12ba0b990
Scott Dixon <scott.dixon@shopify.com>
2021-04-15 13:23:49 +1000

    Read S3 response

--- components/ImageDrop.js
+++ components/ImageDrop.js
@@ -51,29 +51,36 @@ function ImageDrop(props) {
     // Prepare form
     const formData = new FormData()
     const { url, parameters } = staged.stagedUploadsCreate.stagedTargets[0]
-    for (const param of parameters) {
-      formData.append(param.name, param.value);
-    }
-    formData.append('file', file);
+
+    parameters.forEach(({name, value}) => {
+      formData.append(name, value)
+    });
+
+    formData.append('file', file)
 
     // Upload
     const response = await fetch(url, {
       method: 'POST',
       body: formData,
-      mode: 'no-cors'
     });
 
-    // Update collection
-    const key = parameters.find(p => p.name === 'key')
-    collectionUpdate({ variables: {
-        "input": {
-          "id": props.collectionId,
-          "image": {
-            "src": `${url}/${key.value}`
+    if (response.ok) {
+      const key = parameters.find(p => p.name === 'key')
+      collectionUpdate({ variables: {
+          "input": {
+            "id": props.collectionId,
+            "image": {
+              "src": `${url}/${key.value}`
+            }
           }
         }
-      }
-    })
+      })
+      // Todo: handle Shopify API errors
+    } else {
+      console.error('Unable to upload to S3 bucket')
+      setLoading(false)
+    }
+
   }
 
   const [generateUrl, { data }] = useMutation(GENERATE_URL, { onCompleted: uploadFile });
